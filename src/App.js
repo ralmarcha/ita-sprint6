124edfa61afcbf7cafc5130938779cb9b41350fa
ralmarcha <ralmarcha@gmail.com>
2023-02-28 12:55:53 +0100

    exercici 2

--- src/App.js
+++ src/App.js
@@ -2,11 +2,21 @@ import './App.css';
 import Escena from './components/Escena/Escena'
 
 function App() {
+  const props = [
+    "El nostre heroi estava surant per l'espai sideral quan a la llunyania va albirar una nau espacial",
+    "Sentia curiositat per l'interior de la nau i es va posar a inspeccionar-la. Va arribar a una sala amb dues portes.",
+    "L'heroi va decidir travessar la porta que el portava a casa",
+    "Mentrestant, altres herois no van tenir tanta sort en la seva elecció ..."
+  ];
+ const mostrar = () => props.map((element, index) => <p key={index}>{element}</p>);
+
   return (
     <div>
-      <Escena/>
+      <Escena param={mostrar()}/>
     </div>
   );
 }
+
+
 
 export default App;
--- src/components/Escena/Escena.js
+++ src/components/Escena/Escena.js
@@ -1,5 +1,12 @@
 import React from 'react';
+//import {useRef} from 'react';
 
-const Escena = _ => <div>El nostre heroi estava surant per l'espai sideral quan a la llunyania va albirar una nau espacial</div>;
+// const Escena = () => <div>El nostre heroi estava surant per l'espai sideral quan a la llunyania va albirar una nau espacial</div>;
+//const nombre = useRef(null);
+const Escena = props => <div>{props.param}</div>
+
+//useRef?
+//hay que generar un estado al hacer click en el botón
+//un useEffect que cuando cambia el estado cambie el color?
 
 export default Escena;
