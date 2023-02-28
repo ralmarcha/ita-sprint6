85f93c3a85f580330290dbd648016f5da35e93ec
ralmarcha <ralmarcha@gmail.com>
2023-02-28 11:31:02 +0100

    exercici 1

---
+++ src/components/Escena/Escena.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const Escena = _ => <div>El nostre heroi estava surant per l'espai sideral quan a la llunyania va albirar una nau espacial</div>;
+
+export default Escena;
